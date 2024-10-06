a92688aefe1e2e126ba0d3f834283e90f91486fb
trunglecoder <93107952+trunglecoder@users.noreply.github.com>
2024-10-06 20:46:30 +0700

    code chinh thuc

---
+++ src/main/java/com/mycompany/n8proofficial/N8ProOfficial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.n8proofficial;
+
+/**
+ *
+ * @author mac
+ */
+public class N8ProOfficial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
